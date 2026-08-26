31f383de596f33d1ac125803b5151afa2b0cd94a
AmpoluSwapna <ampoluswapna22@gmail.com>
2026-08-26 15:07:54 +0530

    Tokens

---
+++ Day 2.py
@@ -0,0 +1,55 @@
+'''
+>Tokens 
+Tokens are the small unit in python 
+
+identifier
+Identifier is a variable or function or class 
+
+Variable  
+num='python'
+print(type(num))
+
+Function
+def add(a,b)
+    print(a+b)
+add_(4,5)
+
+class
+class details:
+    pass
+per_1= details
+
+keywords 
+Keywords are already saved in python for an specified reason to run
+if, else, for, while ,return ,print
+
+literals
+Every Data types are the literals that need to be stored in variables 
+Ex:
+num=90
+name='swapna'
+
+Operators 
+> +,-,=
+
+Statements in python 
+statements are the instructions given to the program 
+1. num=90
+2. age= 2
+   if age >=18:
+      print(age)
+
+Comments
+Once comments are open the lines inside will never execute in python file
+
+Single line comments(#)
+it is used when the comments are one lined 
+age= 2
+if age >=18: #this check the age is greater or equal 
+print(age)
+
+Multi line comments (''' ''' , """ """)
+it is used to comment more than one comment 
+'''
+
+
