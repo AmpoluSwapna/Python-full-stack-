48093f96d7bd3a0d2b1c3897e195bc34ff54c42b
Your NameAmpoluSwapna <ampoluswapna22@gmail.com>
2026-09-11 19:26:05 +0530

    Introduction of python

---
+++ Day 1.py
@@ -0,0 +1,122 @@
+'''
+What is a program
+program is the process of instructions given by the user or programmer to planning what to do and how to do
+user_en=2000
+bal_=10000
+result=bal_-user_en
+print(result)
+
+procedural language
+An procedural language is organized mainly on functions
+c
+def cal_ad(a,b):
+    return a+b
+a=7
+b=9  
+any_ = cal_ad(a,b)
+print(any_)
+
+Object-oriented programming
+oject-oriented programming is organized mainly on classes and object
+num=7
+print(type(num))
+
+what is python 
+python is a high-level language , interpretable language , dynamically typed language
+python takes care about memory allocation
+num=9
+print(type(num))
+print(id(num)) 
+
+interpretable language 
+it is a line by line execution followed by python that is the reason we call it as interpretable language
+num=9
+num_2=90
+print(num)
+print(type(num))
+print(id(num))
+
+dynamically language
+No need to mention the type of data passing through the variable
+num=78
+any_=['python']
+nums=[1,2]
+all_=(1,2)
+print(type(num))
+print(type(any_))
+print(type(nums))
+print(type(all_))
+
+why python
+contains vast number of libraries
+it is a cross-platform
+it is a open-source
+simple syntax compared to other programming languages
+
+applications of python
+used in data science
+used in web development
+used in AI, ML, Deep learning 
+
+History of python
+it was started in early 1980s, and released in the year of 1991 by Guido Van Rossum
+The name python came from the comedy series called Monty python Circus
+The first version was released in the year 1991 which is python 0.9.0 and now the version we are using 3.14  
+
+Tokens 
+Tokens are the small unit in python 
+---->
+identifier
+Identifier is a variable or function or class
+
+Variable
+num='python'
+print(type(num))
+
+Function
+def add(a,b):
+    print(a+b)
+add(4,5)
+
+class
+class details:
+    pass
+per_1= details
+
+keywords 
+Keywords are already saved in python for an specified reason to run
+if, else, for, while ,return ,print
+
+literals
+Every Data types are the literals that need to be stored in variables 
+Ex:
+num=90
+name='swapna'
+
+Operators 
+> +,-,=
+
+Statements in python 
+statements are the instructions given to the program 
+1. num=90
+2.
+'''
+age = 2
+if age >= 18:
+   print(age)
+'''
+Comments
+Once comments are open the lines inside will never execute in python file
+
+Single line comments(#)
+it is used when the comments are one lined 
+age= 2
+if age >=18: #this check the age is greater or equal 
+print(age)
+
+Multi line comments (''' ''' , """ """)
+it is used to comment more than one comment
+
+Data types &Typeconversion
+'''
+
